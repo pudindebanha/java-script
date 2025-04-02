b52c7707e2a63c6d49d60df5227e30c54328f56b
pudin de banha <micapadua@hotmail.com>
2025-04-02 15:27:25 -0300

    ATT

---
+++ exercicio/ex005/ex001.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}Km/h!`)
+if (vel > 60) { // Condição simples!
+    console.log(`Você ultrapassou a velocidade, MULTADO!`)
+}
+console.log('Use cinto de segurança!')
